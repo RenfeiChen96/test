4990af6a088e813dff9cfa1287866afcbec1018a
Renfei Chen <cnvicmal@gmail.com>
2025-03-01 20:00:46 -0800

    test pr

---
+++ test.py
@@ -0,0 +1,14 @@
+class Test:
+    """
+    DOC_STRING
+    """
+
+    def __init__(self) -> None:
+        """
+        INIT_DOC_STRING
+        """
+
+    def func_name(self) -> RETURN_TYPE:
+        """
+        FUNC_DOC_STRING
+        """
